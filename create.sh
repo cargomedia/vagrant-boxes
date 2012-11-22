ed198131aed0fb27ba84deedee32b6db5c002efd
Reto <reto@cargomedia.ch>
2012-11-22 20:43:15 +0100

    Force "basebox build" for easy re-creation

--- create.sh
+++ create.sh
@@ -2,6 +2,6 @@
 set -e
 cd $(dirname $0)
 
-vagrant basebox build debian-6-amd64
+vagrant basebox build --force debian-6-amd64
 vagrant basebox validate debian-6-amd64
 vagrant basebox export debian-6-amd64

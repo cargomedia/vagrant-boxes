0d7cf45ced6348cda6165e234c34ab4e6d01b925
Reto Kaiser <reto@cargomedia.ch>
2012-12-17 18:23:14 +0100

    Upload basebox to s3 bucket after creation

--- create.sh
+++ create.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
-set -e
+#!/bin/sh -e
 cd $(dirname $0)
 
 vagrant basebox build --force debian-6-amd64
 vagrant basebox validate debian-6-amd64
 vagrant basebox export debian-6-amd64
+
+s3cmd put debian-6-amd64.box s3://s3.cargomedia.ch/vagrant-boxes/
+s3cmd setacl --acl-public --recursive s3://s3.cargomedia.ch/vagrant-boxes/

5e6cd1b24fe84f4759b611fc774243840c2571ad
Reto Kaiser <reto@cargomedia.ch>
2013-07-21 17:07:11 +0200

    Adjust creation script for new veewee version

--- create.sh
+++ create.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -e
 cd $(dirname $0)
 
-vagrant basebox build --force debian-6-amd64
-vagrant basebox validate debian-6-amd64
-vagrant basebox export debian-6-amd64
-
+veewee vbox build --force debian-6-amd64
+veewee vbox validate debian-6-amd64
+/usr/bin/vagrant package --base debian-6-amd64 --output debian-6-amd64.box
 s3cmd put debian-6-amd64.box s3://s3.cargomedia.ch/vagrant-boxes/
+
 s3cmd setacl --acl-public --recursive s3://s3.cargomedia.ch/vagrant-boxes/

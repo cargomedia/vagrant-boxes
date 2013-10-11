54480d4fd18dd11698caf88f38fda572ef0d1c9d
Philipp Gysin <philipp.gysin@cargomedia.ch>
2013-10-11 17:56:41 +0200

    Changes to build debian-6-amd64

--- create.sh
+++ create.sh
@@ -1,13 +1,20 @@
 #!/bin/sh -e
 cd $(dirname $0)
 
-veewee vbox build --force debian-6-amd64
-veewee vbox validate debian-6-amd64
-/usr/bin/vagrant package --base debian-6-amd64 --output debian-6-amd64.box
-s3cmd put debian-6-amd64.box s3://s3.cargomedia.ch/vagrant-boxes/
+#veewee vbox build --force debian-6-amd64
+#veewee vbox validate debian-6-amd64
+#/usr/bin/vagrant package --base debian-6-amd64 --output debian-6-amd64.box
+#s3cmd put debian-6-amd64.box s3://s3.cargomedia.ch/vagrant-boxes/
 
-veewee vbox build --force debian-6-amd64-plain
-/usr/bin/vagrant package --base debian-6-amd64-plain --output debian-6-amd64-plain.box
+#veewee vbox build --force debian-6-amd64-plain
+#/usr/bin/vagrant package --base debian-6-amd64-plain --output debian-6-amd64-plain.box
+
+cd debian-7.1-amd64/
+packer validate debian-7.1-amd64.json
+packer build debian-7.1-amd64.json
+
+exit 0
 s3cmd put debian-6-amd64-plain.box s3://s3.cargomedia.ch/vagrant-boxes/
 
 s3cmd setacl --acl-public --recursive s3://s3.cargomedia.ch/vagrant-boxes/
+
--- debian-7.1-amd64/scripts/zerodisk.sh
+++ debian-7.1-amd64/scripts/zerodisk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 # Zero out the free space to save space in the final image:
-dd if=/dev/zero of=/EMPTY bs=1M count=70000
-rm -f /EMPTY
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -rf /EMPTY

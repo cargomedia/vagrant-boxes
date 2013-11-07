2741c77dfbe04bd4f0c212e7c96d1cfad26c220b
Reto Kaiser <reto@cargomedia.ch>
2013-11-07 09:26:00 +0100

    Streamline virtualbox provisioner

--- debian-6-amd64/scripts/virtualbox.sh
+++ debian-6-amd64/scripts/virtualbox.sh
@@ -13,7 +13,7 @@ if test -f .vbox_version ; then
   aptitude -y install dkms
 
   # Install the VirtualBox guest additions
-  VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+  VBOX_VERSION=$(cat .vbox_version)
   VBOX_ISO=VBoxGuestAdditions_$VBOX_VERSION.iso
   mount -o loop $VBOX_ISO /mnt
   yes|sh /mnt/VBoxLinuxAdditions.run
--- debian-7-amd64/scripts/virtualbox.sh
+++ debian-7-amd64/scripts/virtualbox.sh
@@ -1,4 +1,8 @@
 if test -f .vbox_version ; then
+
+  # Without libdbus virtualbox would not start automatically after compile
+  apt-get -y install --no-install-recommends libdbus-1-3
+
   # The netboot installs the VirtualBox support (old) so we have to remove it
   if test -f /etc/init.d/virtualbox-ose-guest-utils ; then
     /etc/init.d/virtualbox-ose-guest-utils stop
@@ -6,13 +10,7 @@ if test -f .vbox_version ; then
 
   rmmod vboxguest
   aptitude -y purge virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms virtualbox-ose-guest-utils
-
-  # Install dkms for dynamic compiles
-
-  apt-get install -y dkms
-
-  # If libdbus is not installed, virtualbox will not autostart
-  apt-get -y install --no-install-recommends libdbus-1-3
+  aptitude -y install dkms
 
   # Install the VirtualBox guest additions
   VBOX_VERSION=$(cat .vbox_version)
@@ -20,8 +18,6 @@ if test -f .vbox_version ; then
   mount -o loop $VBOX_ISO /mnt
   yes|sh /mnt/VBoxLinuxAdditions.run
   umount /mnt
-
-  /etc/init.d/vboxadd start
 
   # Cleanup Virtualbox
   rm $VBOX_ISO

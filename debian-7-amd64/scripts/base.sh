55a8e49599e4c6c9eea9e60f53d569373bf20f64
Reto Kaiser <reto@cargomedia.ch>
2013-11-07 09:07:21 +0100

    Enable password-less sudo for "vagrant"

--- debian-7-amd64/scripts/base.sh
+++ debian-7-amd64/scripts/base.sh
@@ -3,6 +3,10 @@ apt-get -y update
 apt-get -y install linux-headers-$(uname -r) build-essential
 apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
 apt-get -y install curl unzip
+apt-get clean
+
+# Set up sudo
+echo 'vagrant ALL=NOPASSWD:ALL' > /etc/sudoers.d/vagrant
 
 # Tweak sshd to prevent DNS resolution (speed up logins)
 echo 'UseDNS no' >> /etc/ssh/sshd_config

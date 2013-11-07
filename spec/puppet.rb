900247824c782f3de65bada902df382fbb60709c
Reto Kaiser <reto@cargomedia.ch>
2013-11-07 09:29:26 +0100

    Remove puppet-group hack, add tests

--- spec/puppet.rb
+++ spec/puppet.rb
@@ -3,3 +3,12 @@ require 'spec_helper'
 describe command('puppet --version') do
   it { should return_exit_status 0 }
 end
+
+describe group('puppet') do
+  it { should exist }
+end
+
+describe user('puppet') do
+  it { should exist }
+  it { should belong_to_group 'puppet' }
+end

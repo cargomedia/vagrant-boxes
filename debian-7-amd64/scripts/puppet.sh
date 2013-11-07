2199fb0dfb9b1263aabce44f3ea12a6a020beff4
Reto Kaiser <reto@cargomedia.ch>
2013-11-07 09:16:02 +0100

    Use puppetlabs deb-repo to install puppet on squeeze

--- debian-7-amd64/scripts/puppet.sh
+++ debian-7-amd64/scripts/puppet.sh
@@ -1,6 +1,7 @@
 # Prepare puppetlabs repo
 wget http://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
 dpkg -i puppetlabs-release-wheezy.deb
+rm puppetlabs-release-wheezy.deb
 apt-get update
 
 # Install puppet/facter

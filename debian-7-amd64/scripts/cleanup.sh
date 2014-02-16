6a02dec65dcc58896124e34ebf4bf07d1d1682eb
Reto Kaiser <reto@cargomedia.ch>
2014-02-16 02:05:52 +0100

    Use "printf" instead of "echo" for portability

--- debian-7-amd64/scripts/cleanup.sh
+++ debian-7-amd64/scripts/cleanup.sh
@@ -4,14 +4,14 @@ apt-get -y autoremove
 apt-get -y clean
 
 # Removing leftover leases and persistent rules
-echo "cleaning up dhcp leases"
+echo 'cleaning up dhcp leases'
 rm -f /var/lib/dhcp/*
 
 # Make sure Udev doesn't block our network
-echo "cleaning up udev rules"
+echo 'cleaning up udev rules'
 rm -f /etc/udev/rules.d/70-persistent-net.rules
 rm -rf /dev/.udev/
 rm -f /lib/udev/rules.d/75-persistent-net-generator.rules
 
-echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
-echo "pre-up sleep 2" >> /etc/network/interfaces
+echo 'Adding a 2 sec delay to the interface up, to make the dhclient happy'
+printf '\npre-up sleep 2\n' >> /etc/network/interfaces

ed2d03387e9f4541b91255726953b9a0644cb39c
Reto Kaiser <reto@cargomedia.ch>
2014-04-03 13:46:34 +0200

    Use vagrant_cloud gem

--- lib/vagrant_boxes.rb
+++ lib/vagrant_boxes.rb
@@ -5,15 +5,11 @@ module VagrantBoxes
   require 'pathname'
   require 'fileutils'
   require 'aws-sdk'
-  require 'rest_client'
+  require 'vagrant_cloud'
 
   require 'vagrant_boxes/environment'
   require 'vagrant_boxes/template'
   require 'vagrant_boxes/aws'
-  require 'vagrant_cloud/account'
-  require 'vagrant_cloud/box'
-  require 'vagrant_cloud/version'
-  require 'vagrant_cloud/provider'
 
   def self.logger
     @logger || Logger.new(STDOUT)

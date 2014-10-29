0618b92cde17182f8cc2d3fb3b60dfe8f8ccbb44
Reto Kaiser <reto@cargomedia.ch>
2014-10-29 17:08:01 +0100

    Expect vbox installer exit code 1

--- debian-7-amd64/scripts/virtualbox.sh
+++ debian-7-amd64/scripts/virtualbox.sh
@@ -18,7 +18,7 @@ if test -f .vbox_version ; then
   # Install the VirtualBox guest additions
   VBOX_ISO=VBoxGuestAdditions.iso
   mount -o loop $VBOX_ISO /mnt
-  yes|sh /mnt/VBoxLinuxAdditions.run
+  yes|sh /mnt/VBoxLinuxAdditions.run || [ $? -eq 1 ]
   umount /mnt
 
   # Cleanup Virtualbox

9a841d83210d91fee7eb8c8d14cbcc714ba97d71
ppp0 <philipp.gysin@cargomedia.ch>
2015-10-21 18:20:29 +0200

    Remove wrongly committeds

--- ubuntu-1510/scripts/base.sh
+++
@@ -1,8 +0,0 @@
-# Update the box
-apt-get -y update
-apt-get -y install linux-headers-$(uname -r) build-essential
-apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
-apt-get clean
-
-# Tweak sshd to prevent DNS resolution (speed up logins)
-printf '\nUseDNS no\n' >> /etc/ssh/sshd_config

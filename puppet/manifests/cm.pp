335afbca14ae979f7429c9c8c188210e3373fd62
Reto Kaiser <reto@cargomedia.ch>
2015-11-27 13:21:26 +0100

    Incude cm::services::janus

--- puppet/manifests/cm.pp
+++ puppet/manifests/cm.pp
@@ -2,9 +2,5 @@ node default {
 
   include 'cm::application'
   include 'cm::services'
-
-  include 'janus'
-  include 'janus::plugin::audioroom'
-  include 'janus::plugin::rtpbroadcast'
-
+  include 'cm::services::janus'
 }

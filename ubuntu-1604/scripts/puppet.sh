31496f2fd3e37fe1728da4af602ecf746901cc5d
Reto Kaiser <reto@cargomedia.ch>
2016-05-05 15:49:00 +0200

    Copy 15.04 to 16.04

---
+++ ubuntu-1604/scripts/puppet.sh
@@ -0,0 +1,21 @@
+# Prepare puppetlabs repo
+wget -q http://apt.puppetlabs.com/puppetlabs-release-pc1-vivid.deb -O puppetlabs-release-pc1.deb
+dpkg -i puppetlabs-release-pc1.deb
+rm puppetlabs-release-pc1.deb
+apt-get update
+
+# Install
+apt-get install -qy puppet-agent
+
+# Symlink binaries to PATH
+binaries='puppet facter mco hiera'
+for binary in ${binaries}; do
+  binary_dest="/usr/bin/${binary}"
+  rm -f "${binary_dest}"
+  ln -s "/opt/puppetlabs/bin/${binary}" "${binary_dest}"
+done
+
+# Disable puppet service
+systemctl stop puppet
+rm /lib/systemd/system/puppet.service
+systemctl daemon-reload
---
+++ ubuntu-1604/scripts/virtualbox-vagrant.sh
@@ -0,0 +1,10 @@
+if test -f .vbox_version ; then
+    # Install vagrant keys
+    mkdir -pm 700 /home/vagrant/.ssh
+    curl -kLo /home/vagrant/.ssh/authorized_keys 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+    chmod 0600 /home/vagrant/.ssh/authorized_keys
+    chown -R vagrant:vagrant /home/vagrant/.ssh
+
+    # Install NFS client
+    apt-get -y install nfs-common
+fi
